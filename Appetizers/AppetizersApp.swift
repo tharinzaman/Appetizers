9dff4c91dba026cad7aa1eb64ed0012f7a1a18b7
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2023-12-18 13:13:06 +0000

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Tharin Zaman on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
