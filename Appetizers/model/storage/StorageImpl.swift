9a97e5c211ff858cc702ba575e2c6b7eaa343ce1
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-01-25 18:37:50 +0000

    Test for successfully AppetizerListViewModel.getAppetizers written and passing

--- Appetizers/model/storage/StorageImpl.swift
+++ Appetizers/model/storage/StorageImpl.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-class StorageProtocolImpl: StorageProtocol {
+class StorageImpl: StorageProtocol {
     
     @AppStorage("user") private var userData: Data?
         
---
+++ AppetizersTests/viewmodel/mocks/MockNetworkClientImpl.swift
@@ -0,0 +1,8 @@
+//
+//  MockNetworkClientImpl.swift
+//  AppetizersTests
+//
+//  Created by Tharin Zaman on 25/01/2024.
+//
+
+import Foundation
