f6a2a795abf59db958a417de49793d73d2fc71ad
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-02-06 19:16:23 +0000

    Fetching data with combine added

---
+++ Appetizers/view/screens/CombineScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CombineScreen.swift
+//  Appetizers
+//
+//  Created by Tharin Zaman on 06/02/2024.
+//
+
+import SwiftUI
+
+struct CombineScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CombineScreen()
+}
---
+++ AppetizersUITests/AppetizerListView/AppetizerListViewSuccessTest.swift
@@ -0,0 +1,58 @@
+//
+//  AppetizerListViewTest.swift
+//  AppetizersUITests
+//
+//  Created by Tharin Zaman on 30/01/2024.
+//
+
+import XCTest
+@testable import Appetizers
+
+final class AppetizerListViewSuccessTest: XCTestCase {
+    
+    private var app: XCUIApplication!
+    
+    private var list: XCUIElement!
+    private var listItems: XCUIElementQuery!
+    private var detailView: XCUIElement!
+    
+    override func setUp() {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launchArguments = ["-ui-testing"]
+        app.launchEnvironment = ["-networking-success":"1"]
+        app.launch()
+        
+        list = app.collectionViews["appetizer-list"]
+        let listItemsPredicate = NSPredicate(format: "identifier CONTAINS 'item_'")
+        listItems = list.descendants(matching: .any).containing(listItemsPredicate)
+        
+        detailView = app.descendants(matching: .any)["detail-view"]
+    }
+    
+    override func tearDown() {
+        app = nil
+        list = nil
+        listItems = nil
+        detailView = nil
+    }
+    
+    func test_listContains14Items_tapItemShowDetailView_success() {
+        XCTAssertTrue(list.waitForExistence(timeout: 60))
+        XCTAssertEqual(listItems.count, 14)
+    }
+    
+    func test_tapAppetizerShowDetail() {
+        listItems.firstMatch.tap()
+        XCTAssertTrue(detailView.exists)
+    }
+    
+    func test_tapAddToOrderButton_closeDetailView() {
+        listItems.firstMatch.tap()
+        let addToOrderButton = detailView.buttons[]
+        addToOrderButton.tap()
+        XCTAssertFalse(detailView.exists)
+        XCTAssertTrue(list.exists)
+    }
+
+}
