86e384854065e6c37d79bc468e313ad63e8107fa
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-02-11 14:16:37 +0000

    Testing with xcworkspace and package

---
+++ Framework1/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Framework1",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Framework1",
+            targets: ["Framework1"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Framework1"),
+        .testTarget(
+            name: "Framework1Tests",
+            dependencies: ["Framework1"]),
+    ]
+)
